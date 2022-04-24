f6d3a9858d2b956b58a4e6ec1c3396ecc46a0ff6
Shota Togawa <shotatogawa@ShotanoMacBook-Pro.local>
2022-04-24 18:47:15 +0900

    Revert "add: singup / login component"
    This reverts commit f10869750a31cd56f2431cf977a6ecfcc927882b.

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -2,22 +2,15 @@ import React from "react";
 import HeadTags from "./HeadTags";
 import Navbar from "./Navbar";
 import { Container } from "semantic-ui-react";
-import nProgress from "nprogress";
-import Router from "next/router";
 
-function Layout({ children }) {
-  Router.onRouterChangeStart = () => nProgress.start();
-  Router.onRouterChangeComplete = () => nProgress.done();
-  Router.onRouterChangeError = () => nProgress.done();
+function Layout() {
   return (
     <>
       <HeadTags />
 
       <Navbar />
 
-      <Container style={{ paddingTop: "1rem" }} text>
-        {children}
-      </Container>
+      <Container style={{ paddingTop: "1rem" }} text></Container>
     </>
   );
 }
--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -1,30 +1,7 @@
-import { useRouter } from "next/dist/client/router";
 import React from "react";
-import { Menu, Container, Icon } from 'semantic-ui-react';
-import Link from 'next/link';
 
 function Navbar() {
-  const router = useRouter()
-  const isActive = router => router.pathname === router;
-  return (
-    <Menu fluid borderless>
-      <Container text>
-        <Link href="/login">
-          <Menu.Item header active={isActive('/login')}>
-          <Icon size="large" name="sign in" />
-            Login
-          </Menu.Item>
-        </Link>
-
-        <Link href="/signup">
-          <Menu.Item header active={isActive('/signup')}>
-          <Icon size="large" name="signup" />
-            Signup
-          </Menu.Item>
-        </Link>
-      </Container>
-    </Menu>
-  )
+  return <div></div>;
 }
 
 export default Navbar;
